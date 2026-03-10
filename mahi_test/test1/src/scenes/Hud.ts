b1382945c2bfe60557e2defbda5aed14a1991ca6
aam_user <punktiktok@gmail.com>
2026-03-10 18:46:21 +0100

    Aggiornato Gioco

---
+++ mahi_test/test1/src/scenes/Hud.ts
@@ -0,0 +1,20 @@
+import GamePlay from "./GamePlay";
+
+export default class Hud extends Phaser.Scene {
+
+  /** Scene constructor (key: Hud). */
+  constructor() {
+    super({
+      key: "Hud",
+    });
+  }
+
+  /** HUD scene entry point (currently unused). */
+  create() {
+    console.log("HUD");
+
+  }
+
+
+
+}
