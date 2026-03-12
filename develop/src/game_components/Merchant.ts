b4f4d34adb031414d3d21537fbae67950d2bd8a3
gioca <giocaldarelli08@gmail.com>
2026-03-12 10:02:01 +0100

    Commit iniziale merchant

---
+++ develop/src/game_components/Merchant.ts
@@ -0,0 +1,11 @@
+import Phaser from "phaser";
+import Actor from "./Actor";
+
+export default class Merchant extends Actor {
+
+    private _ragioIterazione: number;
+    private _prompt: Phaser.GameObjects.Text;
+    private _shopAperto: boolean;
+
+    
+}
---
+++ develop/src/scenes/ShopUI.ts
@@ -0,0 +1,14 @@
+export default class ShopUI extends Phaser.Scene {
+
+  constructor() {
+    super({ key: "ShopUI" });
+  }
+
+  create() {
+    console.log("ShopUI");
+  }
+
+  update(time: number, delta: number): void {
+
+  }
+}
