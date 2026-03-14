505cdaa9716c8be1bbd508bc7f1eb0d30323f8d3
Sshs <omniahiba05@gmail.com>
2026-03-14 02:15:04 +0100

    Aggiunta menu e game over

---
+++ develop/public/assets/tilemaps/readMe.txt
@@ -0,0 +1,8 @@
+livello 0: floor
+livello 1: holes 
+livello 2: walls
+livello 3: decorations
+
+proprietà:
+collide
+death
--- develop/src/GameData.ts
+++ develop/src/GameData.ts
@@ -51,6 +51,7 @@ export let GameData: gameData = {
   ],
 
   images: [
+    { name: "main_logo", path: "assets/images/logos/logo_develop.jpg" },
     { name: "game_logo", path: "assets/images/logos/game_logo.png" },
     { name: "silent_production_logo", path: "assets/images/logos/logo_silent_production_scontornato.png" },
 
@@ -72,9 +73,7 @@ export let GameData: gameData = {
   ],
 
   videos: [
-
-    // { name: "video", path: "/assets/video/video.mp4" },
-
+    { name: "logovideo", path: "assets/logovideo.mp4" },
   ],
 
   audios: [
